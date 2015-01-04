14bd9575689583ac8593b965d5a0ba4569c49ea3
RickySSD <ricky6982@gmail.com>
2015-01-04 00:39:24 -0300

    Se creo la funcion Agregar Persona.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- src/StorageRoom/AppBundle/Controller/PersonaController.php
+++ src/StorageRoom/AppBundle/Controller/PersonaController.php
@@ -3,6 +3,7 @@
 namespace StorageRoom\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 use StorageRoom\AppBundle\Entity\Persona;
 use StorageRoom\AppBundle\Form\Type\PersonaType;
@@ -19,5 +20,25 @@ class PersonaController extends Controller
             ));
     }
 
+    public function crearAction(Request $request)
+    {
+        $persona = new Persona();
+        $formPersona = $this->createForm(new PersonaType, $persona);
+        $formPersona->handleRequest($request);
+        if ($formPersona->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($persona);
+            $em->flush();
 
+            $request->getSession()->getFlashBag()->add(
+                    'success', 'Se Creo una Persona Correctamente'
+                );
+        }else{
+            $request->getSession()->getFlashBag()->add(
+                    'error', 'No se pudo Crear la Persona'
+                );
+        }
+
+        return $this->redirect($this->generateUrl('persona_homepage'));
+    }
 }
